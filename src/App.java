93b1df748ba21fd10a1983b1f5c31ad46b175b38
qiwangthi <grace219feb@gmail.com>
2024-03-25 14:47:13 +0100

    aufgabe2

--- src/App.java
+++ src/App.java
@@ -2,4 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+    /*Studierender Qi = new Studierender("Qi", "00153608", "UXD");
+    Doziernder Nestler = new Dozierender(name:"Nestler")*/
 }
--- src/Studierender.java
+++ src/Studierender.java
@@ -1,3 +1,36 @@
+// Klasse Studierender
 public class Studierender {
+    // Datenfelder
+  private String name;
+  private String matrikelnummer;
+  private String studiengang;
 
+  // Konstruktor
+  public Studierender(String name,String matrikelnummer,String studiengang){
+    this.name = name;
+    this.matrikelnummer = matrikelnummer;
+    this.studiengang = studiengang;
+  }
+//Öffentliche Methode
+  public  String frageStellen (Dozierender dozierender,String frage){
+    return frage;
+  }
+  public String getName(){
+    return this.name;
+  }
+  public String getMatrikelnummer(){
+    return this.matrikelnummer;
+  }
+  public String getStudiengang(){
+    return this.studiengang;
+  }
+
+  /*
+// Methode , um eine Sprechstundenanfrage an einen Dozierenden zu senden
+   public void sprechstundenAnfrageSenden(Dozierender dozierender,String grund)
+   System.out.println("Sprechstundenanfrage an" + doziernder.getName() + "gesendet: " + grund);
+   */
+
+
+  
 }
