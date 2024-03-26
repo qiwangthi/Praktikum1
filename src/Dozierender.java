7bcf73906d297eee9d35f98d6fcd89d25fe9ca03
srgndrt <srgndsgn@yandex.com>
2024-03-26 09:15:57 +0100

    Update Dozierender.java

--- src/Dozierender.java
+++ src/Dozierender.java
@@ -1,3 +1,50 @@
 public class Dozierender {
 
+    private String name;
+    private String fakultaet;
+    private String bueronummer;
+    private String feedback;
+
+    public Dozierender(String name, String fakultaet, String bueronummer){
+        this.name = name;
+        this.fakultaet = fakultaet;
+        this.bueronummer = bueronummer;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setFakultaet(String fakultaet){
+        this.fakultaet = fakultaet;
+    }
+
+    public String getFakultaet(){
+        return fakultaet;
+    }
+
+    public void setBueronummer(String bueronummer){
+        this.bueronummer = bueronummer;
+    }
+
+    public String getBueronummer(){
+        return bueronummer;
+    }
+
+    public void setFeedback(String feedback){
+        this.feedback = feedback;
+    }
+
+    public String getFeedback(){
+        return feedback;
+    }
+
+    public void frageBeantworten(Studierender studierender, String antwort){
+
+    }
+    ;
 }
