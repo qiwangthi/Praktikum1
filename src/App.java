8963d052929271a477197c96655abe72ce552853
qiwangthi <164479452+qiwangthi@users.noreply.github.com>
2024-04-07 18:06:05 +0200

    aufgabe3

--- src/App.java
+++ src/App.java
@@ -1,13 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+      
+    public static void main(String[] args){
     //erzeug ein Studierender
     Studierender studierender = new Studierender("Qi", "00153608", "UXD");
     //erzeug ein Dozierender
     Doziernder dozierender = new Dozierender(name:"Nestler","UXD","C303");
     //frage stellen
-    Dozierender.fragestellen(doziernder,"wann haben wir praktikum? ")
+    dozierender.fragestellen(doziernder.getName(),"wann haben wir praktikum? ")
     
    
     
