c15b7c1204cead49d292d7370709fdf81dc7ff9a
qiwangthi <164479452+qiwangthi@users.noreply.github.com>
2024-04-10 20:01:46 +0200

    Update Dozierender.java

--- src/Dozierender.java
+++ src/Dozierender.java
@@ -1,40 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Dozierender {
-//Daten field
-private String name;
-private String fakultaet;
-private String bueronummer;
-private List<String> feedbacks;
-
-//konstruktor
-public Dozierender (String name,String fakultaet,String bueronummer){
-    this.name = name;
-    this.fakultaet = fakultaet;
-    this.bueronummer = bueronummer;
-    this.feedbacks = new ArrayList<>();
-}
-
-//get-Methode
-public String getName(){
-    return this.name;
-}
-public String getFakultaet(){
-    return this.fakultaet;
-}
-public String getBueronummer(){
-    return this.bueronummer;
-}
-//Öffentliche Methode:
-public void frageBeantworten(Studierender student,String antwort){
-System.out.println("von:"+ getName());
-System.out.println("an:"+ student);
-System.out.println("Antwort:"+ antwort);
-}
-//feedback von Donzent
-public void feedbackVerwalten(Feedback feedback)
-}
 
     private String name;
     private String fakultaet;
